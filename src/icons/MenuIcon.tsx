ed1c547ca3babdd052e25572c514069d5c2cd44f
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-08 23:45:20 -0300

    add custom svg icons

---
+++ src/icons/MenuIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MenuIcon = (): React.ReactElement => (
+  <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M26.4 13.8V16.2H4.79999V13.8H26.4Z" fill="white" />
+    <path d="M26.4 6.59998V8.99997H4.79999V6.59998H26.4Z" fill="white" />
+    <path d="M26.4 21V23.4H4.79999V21H26.4Z" fill="white" />
+  </svg>
+);
+export default MenuIcon;
