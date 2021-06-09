eb1777f654be6dcf2f110379431e615c48b91416
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-08 23:46:33 -0300

    customize list item components

--- src/customTheme.ts
+++ src/customTheme.ts
@@ -4,5 +4,28 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: 'Manrope',
   },
+  palette: {
+    primary: { main: '#2C2D35' },
+    secondary: { main: '#E5E5E5' },
+  },
+  overrides: {
+    MuiListItem: {
+      root: {
+        background: 'transparent',
+        '&$selected': {
+          height: '50px',
+          backgroundColor: 'rgba(255, 82, 70, 0.25)',
+          borderLeft: '5px solid #FF5246',
+        },
+      },
+    },
+    MuiListItemText: {
+      primary: {
+        fontWeight: 600,
+        fontSize: '16px',
+        lineHeight: '27px',
+      },
+    },
+  },
 });
 export default theme;
