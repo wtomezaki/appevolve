eec807c21cbeae14bcd7fc27581762602a625b28
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-08 21:38:13 -0300

    using react-query and material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { memo } from 'react';
 import './App.css';
+import Routes from './routes';
+import { ThemeProvider } from '@material-ui/core/styles';
+import customTheme from './customTheme';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-function App(): React.ReactElement {
+const queryClient = new QueryClient();
+
+const App: React.FC = memo(() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <div className="App">
+          <Routes />
+        </div>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
-}
+});
 
 export default App;
