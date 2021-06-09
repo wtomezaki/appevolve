2567964a1a7f2c1b2f72c72dd3784c8257e98022
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-08 23:48:46 -0300

    fix missing return type on logo icon

--- src/icons/LogoIcon.tsx
+++ src/icons/LogoIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LogoIcon = () => {
+const LogoIcon = (): React.ReactElement => {
   return (
     <svg width="160" height="31" viewBox="0 0 160 31" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
