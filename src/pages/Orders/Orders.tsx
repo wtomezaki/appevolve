254e0f7d4d68eb6240d1c90521456db886f3fb18
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-08 21:38:02 -0300

    creating routes

---
+++ src/pages/Orders/Orders.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Orders = (): React.ReactElement => {
+  return <></>;
+};
+
+export default Orders;
---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement, memo } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Dashboard, Orders } from './pages';
+
+const Routes = memo(
+  (): ReactElement => (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/orders" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  )
+);
+
+export default Routes;
