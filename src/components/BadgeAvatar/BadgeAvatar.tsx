30b58eb0b76b5e75f560b2cfe97737eace458153
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-09 21:05:30 -0300

    badgeavatar component

---
+++ src/components/BadgeAvatar/BadgeAvatar.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Theme, makeStyles, withStyles, createStyles } from '@material-ui/core/styles';
+import { Badge, Avatar } from '@material-ui/core';
+
+const StyledBadge = withStyles((theme: Theme) =>
+  createStyles({
+    badge: {
+      height: '12px',
+      width: '12px',
+      backgroundColor: '#55A3AE',
+      borderRadius: '100%',
+      color: '#55A3AE',
+    },
+  })
+)(Badge);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  })
+);
+
+interface Props {
+  name: string;
+  imgSrc: string;
+}
+
+const BadgeAvatar = ({ name, imgSrc }: Props): React.ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <StyledBadge
+        overlap="circle"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        variant="dot"
+      >
+        <Avatar alt={name} src={imgSrc} />
+      </StyledBadge>
+    </div>
+  );
+};
+
+export default BadgeAvatar;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as PageTemplate } from './PageTemplate/PageTemplate';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as Topbar } from './Topbar/Topbar';
+export { default as BadgeAvatar } from './BadgeAvatar/BadgeAvatar';
